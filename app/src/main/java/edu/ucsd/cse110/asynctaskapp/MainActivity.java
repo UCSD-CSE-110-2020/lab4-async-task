61b5534688cbafda486e966b8d266b6737affe78
sshokooh <sshokooh@ucsd.edu>
2020-01-29 11:26:08 -0800

    Added background thread to fix app (ex 1)

--- app/src/main/java/edu/ucsd/cse110/asynctaskapp/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/asynctaskapp/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.asynctaskapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean isCancelled = false;
+    private Boolean isCancelled = false;
     private Button btnCount;
     private Button btnCancel;
     private TextView textResult;
@@ -25,20 +26,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 isCancelled = false;
+                CountToTenAsyncTask runner = new CountToTenAsyncTask();
+                runner.execute();
 
-                for (int i = 0; i < 10 && !isCancelled; i++) {
-                    textResult.setText(String.valueOf(i));
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (isCancelled) {
-                    textResult.setText("Cancelled");
-                } else {
-                    textResult.setText("10");
-                }
             }
         });
 
@@ -50,4 +40,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private class CountToTenAsyncTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            for (int i = 0; i < 10 && !isCancelled; i++) {
+                publishProgress(String.valueOf(i));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (isCancelled.equals("true")) {
+
+            } else {
+                publishProgress("10");
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+
+        }
+        @Override
+        protected void onPreExecute(){
+
+        }
+        @Override
+        protected void onProgressUpdate(String... text){
+            textResult.setText(text[0]);
+        }
+    }
 }
+
+
